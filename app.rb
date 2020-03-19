3b93e46ea77616195b16baaae95cbbed3e2aea3f
nmehrotra613 <neham101@gmail.com>
2020-03-19 08:37:37 +0000

    Work So Far

--- app.rb
+++ app.rb
@@ -3,8 +3,9 @@ require "sinatra"                                                               
 require "sinatra/reloader" if development?                                            #
 require "sequel"                                                                      #
 require "logger"                                                                      #
-require "twilio-ruby"                                                                 #
-require "bcrypt"                                                                      #
+require "twilio-ruby"   
+require "geocoder"                                                              #
+require "bcrypt"                                                                     #
 connection_string = ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/development.sqlite3"  #
 DB ||= Sequel.connect(connection_string)                                              #
 DB.loggers << Logger.new($stdout) unless DB.loggers.size > 0                          #
@@ -14,5 +15,34 @@ before { puts; puts "--------------- NEW REQUEST ---------------"; puts }       
 after { puts; }                                                                       #
 #######################################################################################
 
-events_table = DB.from(:events)
-rsvps_table = DB.from(:rsvps)
+tourist_locations_table = DB.from(:tourist_locations)
+reviews_table = DB.from(:reviews)
+users_table = DB.from(:users)
+
+get "/" do 
+    @tourist_locations = tourist_locations_table.all.to_a
+    pp @tourist_locations 
+    view "touristlocations"
+end 
+
+get "/touristlocations/:id" do 
+    puts "params: #{params}"
+
+    @tourist_location = tourist_locations_table.where(id: params[:id]).to_a[0]
+    location_name = @tourist_location[:location_name]
+    results = Geocoder.search(location_name)
+    lat_lng = results.first.coordinates
+    @lat = lat_lng[0]
+    @lng = lat_lng [1]
+
+    pp @tourist_location
+    @reviews = reviews_table.where(tourist_locations_id: @tourist_location[:id]).to_a
+
+    view "touristlocation"
+end 
+
+get "/touristlocations/:id/reviews/new" do
+    
+    @tourist_location = tourist_locations_table.where(id: params[:id]).to_a[0]
+    view "newreview"
+end 
--- createdb.rb
+++ createdb.rb
@@ -5,31 +5,40 @@ DB = Sequel.connect(connection_string)                                          
 #######################################################################################
 
 # Database schema - this should reflect your domain model
-DB.create_table! :events do
+
+DB.create_table! :tourist_locations do
   primary_key :id
-  String :title
-  String :description, text: true
-  String :date
-  String :location
+  String :location_name
+  String :description, text: true 
+  String :top_things, text: true
 end
-DB.create_table! :rsvps do
+
+DB.create_table! :reviews do
   primary_key :id
-  foreign_key :event_id
-  Boolean :going
-  String :name
-  String :email
+  foreign_key :tourist_locations_id
+  foreign_key :user_id 
+  String :hidden_gem
+  String :top_place
+  String :tourist_trap 
+  String :travel_style
   String :comments, text: true
 end
 
+  DB.create_table! :user do 
+    primary_key :id 
+    String :name 
+    String :email 
+    String :passowrd 
+end
+
 # Insert initial (seed) data
-events_table = DB.from(:events)
+tourist_locations_table = DB.from(:tourist_locations)
 
-events_table.insert(title: "Bacon Burger Taco Fest", 
-                    description: "Here we go again bacon burger taco fans, another Bacon Burger Taco Fest is here!",
-                    date: "June 21",
-                    location: "Kellogg Global Hub")
-
-events_table.insert(title: "Kaleapolooza", 
-                    description: "If you're into nutrition and vitamins and stuff, this is the event for you.",
-                    date: "July 4",
-                    location: "Nowhere")
+tourist_locations_table.insert(location_name: "New Orleans", 
+                    description: "New Orleans is world-renowned for its distinct music, Creole cuisine, unique dialect, and its annual celebrations and festivals, most notably Mardi Gras.",
+                    top_things: "French Quarter, Bourbon Street, Jackson Square")
+                 
+tourist_locations_table.insert(location_name: "Nashville", 
+                    description: "Nashville is the capital of the U.S. state of Tennessee. It is located on the Cumberland River in Davidson County. Nicknamed 'Music City, U.S.A.', Nashville is the home of the Grand Ole Opry, the Country Music Hall of Fame, and many major record labels.",
+                    top_things: "Grand Ole Opry, Country Music Hall of Fame, The Parthenon") 
+                   
